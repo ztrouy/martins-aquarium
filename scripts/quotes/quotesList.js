5dd7584a7ab28caed1ba322aaa5581a4496071bf
Zachary Trouy <34666901+ztrouy@users.noreply.github.com>
2024-01-19 16:17:04 -0600

    First pass on quotesList.js

--- scripts/quotes/quotesList.js
+++ scripts/quotes/quotesList.js
@@ -5,10 +5,21 @@ export const quotesList = () => {
     const quotes = getQuotes()
     
     // Start building a string of each quote
-    let htmlString = `<article class="quotes-list">`
+    let htmlString = `<article class="quotes-list">
+        <div class="quotes__header">Quotes</div>`
 
     // Create HTML representations of each quote here
     for (const quote of quotes) {
-        
+        htmlString += `<div class="quotes__item">`
+
+        htmlString += `<div class="quotes__name">${quote.name}</div>`
+
+        htmlString += `<div class="quotes__text">${quote.quote}</div>`
+
+        htmlString += `</div>`
     }
+
+    htmlString += `</article>`
+
+    return htmlString
 }
