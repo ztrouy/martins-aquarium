8f59a3a6f2ed41f8eb13014206b4a8aef02b0783
Zachary Trouy <34666901+ztrouy@users.noreply.github.com>
2024-01-19 16:41:46 -0600

    Added import to fix bug with injecting locationsList

--- scripts/main.js
+++ scripts/main.js
@@ -2,6 +2,7 @@ import { navList } from "./nav/navList.js"
 import { fishList } from "./fish/fishList.js"
 import { tipsList } from "./tips/tipList.js"
 import { quotesList } from "./quotes/quotesList.js"
+import { locationsList } from "./locations/locationsList.js"
 
 const navHTMLElement = document.querySelector("#nav")
 navHTMLElement.innerHTML = navList()
